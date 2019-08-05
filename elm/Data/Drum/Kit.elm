8bbb974e7ba2917f74f165df1eee4dc887a14dd9
Nicolas Perriault <nicolas@perriault.net>
2019-08-05 15:52:23 +0200

    Leverage parcel static assets path configuration.

--- elm/Data/Drum/Kit.elm
+++ elm/Data/Drum/Kit.elm
@@ -28,113 +28,113 @@ pick =
 randomClap : Generator String
 randomClap =
     pick
-        [ "audio/clap-808.ogg"
-        , "audio/clap-analog.ogg"
-        , "audio/clap-crushed.ogg"
-        , "audio/clap-fat.ogg"
-        , "audio/clap-slapper.ogg"
-        , "audio/clap-tape.ogg"
+        [ "clap-808.ogg"
+        , "clap-analog.ogg"
+        , "clap-crushed.ogg"
+        , "clap-fat.ogg"
+        , "clap-slapper.ogg"
+        , "clap-tape.ogg"
         ]
 
 
 randomCrash : Generator String
 randomCrash =
     pick
-        [ "audio/crash-808.ogg"
-        , "audio/crash-acoustic.ogg"
-        , "audio/crash-noise.ogg"
-        , "audio/crash-tape.ogg"
+        [ "crash-808.ogg"
+        , "crash-acoustic.ogg"
+        , "crash-noise.ogg"
+        , "crash-tape.ogg"
         ]
 
 
 randomHihat : Generator String
 randomHihat =
     pick
-        [ "audio/hihat-808.ogg"
-        , "audio/hihat-digital.ogg"
-        , "audio/hihat-electro.ogg"
-        , "audio/hihat-plain.ogg"
+        [ "hihat-808.ogg"
+        , "hihat-digital.ogg"
+        , "hihat-electro.ogg"
+        , "hihat-plain.ogg"
         ]
 
 
 randomKick : Generator String
 randomKick =
     pick
-        [ "audio/kick-deep.ogg"
-        , "audio/kick-electro01.ogg"
-        , "audio/kick-floppy.ogg"
-        , "audio/kick-newwave.ogg"
-        , "audio/kick-oldschool.ogg"
-        , "audio/kick-tape.ogg"
-        , "audio/kick-tight.ogg"
-        , "audio/kick-tron.ogg"
+        [ "kick-deep.ogg"
+        , "kick-electro01.ogg"
+        , "kick-floppy.ogg"
+        , "kick-newwave.ogg"
+        , "kick-oldschool.ogg"
+        , "kick-tape.ogg"
+        , "kick-tight.ogg"
+        , "kick-tron.ogg"
         ]
 
 
 randomOpenhat : Generator String
 randomOpenhat =
     pick
-        [ "audio/openhat-808.ogg"
-        , "audio/openhat-acoustic01.ogg"
-        , "audio/openhat-analog.ogg"
-        , "audio/openhat-slick.ogg"
-        , "audio/openhat-tight.ogg"
+        [ "openhat-808.ogg"
+        , "openhat-acoustic01.ogg"
+        , "openhat-analog.ogg"
+        , "openhat-slick.ogg"
+        , "openhat-tight.ogg"
         ]
 
 
 randomPerc : Generator String
 randomPerc =
     pick
-        [ "audio/perc-808.ogg"
-        , "audio/perc-chirpy.ogg"
-        , "audio/perc-hollow.ogg"
-        , "audio/perc-laser.ogg"
-        , "audio/perc-metal.ogg"
-        , "audio/perc-nasty.ogg"
-        , "audio/perc-short.ogg"
-        , "audio/perc-tambo.ogg"
-        , "audio/perc-tribal.ogg"
-        , "audio/perc-weirdo.ogg"
+        [ "perc-808.ogg"
+        , "perc-chirpy.ogg"
+        , "perc-hollow.ogg"
+        , "perc-laser.ogg"
+        , "perc-metal.ogg"
+        , "perc-nasty.ogg"
+        , "perc-short.ogg"
+        , "perc-tambo.ogg"
+        , "perc-tribal.ogg"
+        , "perc-weirdo.ogg"
         ]
 
 
 randomRide : Generator String
 randomRide =
     pick
-        [ "audio/ride-acoustic01.ogg"
-        , "audio/ride-acoustic02.ogg"
+        [ "ride-acoustic01.ogg"
+        , "ride-acoustic02.ogg"
         ]
 
 
 randomShaker : Generator String
 randomShaker =
     pick
-        [ "audio/shaker-analog.ogg"
-        , "audio/shaker-shuffle.ogg"
-        , "audio/shaker-suckup.ogg"
+        [ "shaker-analog.ogg"
+        , "shaker-shuffle.ogg"
+        , "shaker-suckup.ogg"
         ]
 
 
 randomSnare : Generator String
 randomSnare =
     pick
-        [ "audio/snare-electro.ogg"
-        , "audio/snare-lofi01.ogg"
-        , "audio/snare-noise.ogg"
-        , "audio/snare-pinch.ogg"
+        [ "snare-electro.ogg"
+        , "snare-lofi01.ogg"
+        , "snare-noise.ogg"
+        , "snare-pinch.ogg"
         ]
 
 
 randomTom : Generator String
 randomTom =
     pick
-        [ "audio/tom-808.ogg"
-        , "audio/tom-acoustic01.ogg"
-        , "audio/tom-acoustic02.ogg"
-        , "audio/tom-analog.ogg"
-        , "audio/tom-chiptune.ogg"
-        , "audio/tom-fm.ogg"
-        , "audio/tom-lofi.ogg"
-        , "audio/tom-rototom.ogg"
-        , "audio/tom-short.ogg"
+        [ "tom-808.ogg"
+        , "tom-acoustic01.ogg"
+        , "tom-acoustic02.ogg"
+        , "tom-analog.ogg"
+        , "tom-chiptune.ogg"
+        , "tom-fm.ogg"
+        , "tom-lofi.ogg"
+        , "tom-rototom.ogg"
+        , "tom-short.ogg"
         ]
